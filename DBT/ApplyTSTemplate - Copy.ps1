c3f2b35cb69b0560b31fe0e3f69f5bb3cebf0cc1
PompeyNYC <109304173+PompeyNYC@users.noreply.github.com>
2022-07-14 10:10:00 -0700

    Added First Version
    RMS SSIS Import Job Version 1

---
+++ DBT/ApplyTSTemplate - Copy.ps1
@@ -0,0 +1,23 @@
+$database = "\\XENODEMOUS27\RMS"
+$path = "C:\Client\RA\DataFeeds\RMS\DBT\RMS"
+
+function ApplyTSTemplate( [string] $Database, [string] $Path)
+{
+	Import-Module 'C:\Windows\Microsoft.NET\assembly\GAC_MSIL\Xenomorph.TimeScape.PowerShell\v4.0_5.0.4000.0__2adfa95c2785233e\Xenomorph.TimeScape.PowerShell.dll'
+
+    if(!(Test-Path $path))
+    {
+        write-error "No database template found at $path"
+        return
+    }
+
+    try {Copy-Database $Path $Database -SourceIsXML}
+    catch
+    {
+        write-host "Failed to apply template to database"
+        $ErrorMessage = $_.Exception.Message
+        write-host $ErrorMessage
+    }
+}
+
+ApplyTSTemplate -Database $database -Path $path
